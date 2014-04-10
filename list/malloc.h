fd7dd12bc7e32b078f7e6fb4fd79478932886f5a
Arclights <tommy.kvant@gmail.com>
2014-04-10 22:48:29 +0200

    Initial commit

---
+++ list/malloc.h
@@ -0,0 +1,9 @@
+#include <stddef.h>
+
+void* malloc1(size_t size);
+void* calloc1(size_t nitems, size_t size);
+void* realloc1(void *ptr, size_t size);
+void free1(void* ptr);
+
+void check_all_freed();
+void print_list();
